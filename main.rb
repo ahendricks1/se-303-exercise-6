29db3dca015c4a701c4b007b6450fb04f74d1b32
Austin Hendricks <hendraus@oregonstate.edu>
2021-05-06 15:31:51 -0700

    Preserve whole object in button details class

--- main.rb
+++ main.rb
@@ -15,3 +15,14 @@ end
 def lighten_foreground(label_text, x, y, foreground_color)
   paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
 end
+
+class ButtonDetails
+  def initialize(label_text, x, y, foreground_color)
+    @label_text = label_text
+    @x = x
+    @y = y
+    @foreground_color = foreground_color
+  end
+
+
+end
